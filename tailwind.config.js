90c0f14e1360e7ac36d02d20a1df8943b382f7ee
Ayush1M <worklife2442@gmail.com>
2023-04-20 01:25:01 +0530

    added theme for width and extend values for box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,21 @@ module.exports = {
      'dark-grayish-blue' : 'hsl(210, 10%, 33%)',
      'grayish-blue': 'hsl(201, 11%, 66%)',
      'white' : '#fff',
+     'light-gray' : 'rgba(203, 204, 204, .25)',
     },
     fontFamily : {
       fontBai : ['Bai Jamjuree', 'sans-serif']
     },
-    extend: {},
+    minWidth : {
+      's' : '360px',
+      'l' : '680px',
+    },
+    extend: {
+      boxShadow : {
+        'cyan' : '0 2px 6px hsl(171, 66%, 44%)',
+        'blue' : '0 2px 6px hsl(233, 100%, 69%)',
+      }
+    },
   },
   plugins: [],
 }
